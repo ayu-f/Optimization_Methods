cd9c82375c43332eccf3de1346079e9397d765bc
Rushan Gabdushev <2rushan2@gmail.com>
2022-02-21 21:14:35 +0300

    Input

--- method/main.py
+++ method/main.py
@@ -1,5 +1,16 @@
 import method.simplex
+import method.input_general
+
+from method.input_general import input_general
+from method.input_general import input_general_f
+from
 
 if __name__ == '__main__':
     a = 0
 
+
+def main():
+    signed_indexes, c1, A1, b1, A2, b2, A3, b3  = input_general_f("input.txt")
+    print("success")
+
+main()
