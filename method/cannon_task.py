540737d423482c9d4b9f969517f1a1bc41e9dfef
Alexey <fisyuk.ayu@edu.spbstu.ru>
2022-02-21 23:31:16 +0300

    dual+canon

---
+++ method/cannon_task.py
@@ -0,0 +1,92 @@
+import string
+
+from dual_task import copyVec, copyMatr
+
+
+def addSupportiveBasicValue(A : list, index : int, coef : float):
+    for i in range(len(A)):
+        if i == index:
+            A[i].append(coef)
+        else:
+            A[i].append(0)
+    return
+
+
+def parse_to_canon(A : list, b : list, c : list, v : float, sign_limits : list, type_opt : string, value_limits : list):
+    # init matrixes and vectors
+    A_c = copyMatr(A)
+    b_c = copyVec(b)
+    N = list()
+    B = list()
+    c_c = list()
+    v_c = 0
+    originalSize = len(c)
+    originalVars = list()
+
+    # first, remake target-function
+    if type_opt == "min":
+        c_c = [coef * -1 for coef in c]
+        v_c = v * -1
+    else:
+        c_c = [coef for coef in c]
+        v_c = v
+
+    for i in range(len(sign_limits)):
+        if sign_limits[i] == "<=":
+            B.append(len(c_c))
+            addSupportiveBasicValue(A_c, i, 1)
+            c_c.append(0)
+        elif sign_limits[i] == ">=":
+            B.append(len(c_c))
+            addSupportiveBasicValue(A_c, i, -1)
+            c_c.append(0)
+
+    for i in range(originalSize):
+        if i not in value_limits:
+            for A_line in A_c:
+                A_line.append(A_line[i] * -1)
+            c_c.append(-1 * c_c[i])
+            originalVars.append([i, i, len(c_c) - 1])
+
+    for i in range(len(c_c)):
+        if i not in B:
+            N.append(i)
+    return N, B, A_c, b_c, c_c, v_c, originalSize, originalVars
+
+
+def print_task_as_canon(A : list, b : list, c : list, v : float):
+    str_to_out = "max: z = " + str(v)
+    for i in range(len(c)):
+        str_to_out += " + " + str(c[i]) + " * x" + str(i+1)
+    print(str_to_out)
+
+    for i in range(len(A)):
+        str_to_out = str(b[i]) + " = "
+        for j in range(len(A[i])):
+            str_to_out += str(A[i][j]) + " * x" + str(j+1)
+            if j != len(A[i]) - 1:
+                str_to_out += " + "
+        print(str_to_out)
+
+    str_to_out = ""
+    for i in range(len(c)):
+        if i != len(c) + len(A) - 1:
+            str_to_out += "x" + str(i + 1) + ", "
+        else:
+            str_to_out += "x" + str(i + 1) + " >= 0"
+            print(str_to_out)
+    return
+
+
+def convert_canon_solution_to_original(canon_sol : list, originalSize : int, originalVars : list):
+    original_sol = list()
+    original_indexes = [origVar[0] for origVar in originalVars]
+
+    for i in range(originalSize):
+        if i not in original_indexes:
+            original_sol.append(canon_sol[i])
+        else:
+            for j in range(len(originalVars)):
+                if i == originalVars[j][0]:
+                    original_sol.append(canon_sol[originalVars[j][1]] - canon_sol[originalVars[j][2]])
+    return original_sol
---
+++ method/dual_task.py
@@ -0,0 +1,96 @@
+import string
+
+
+def copyMatr(A: list):
+    A_copy = list()
+    for i in range(len(A)):
+        A_copy.append([A[i][j] for j in range(len(A[i]))])
+    return A_copy
+
+
+def copyVec(A: list):
+    A_copy = list()
+    for i in range(len(A)):
+        A_copy.append(float(A[i]))
+    return A_copy
+
+
+def takeColumn(matrix: list, indx: int):
+    return [matrix_line[indx] for matrix_line in matrix]
+
+
+def to_dual_task(A: list, b: list, c: list, sign_limits: list, value_limits, type_opt: string):
+    A_dual = list()
+    limSigns_dual = list()
+    valuesLimits_dual = list()
+
+    limitsCnt = len(sign_limits)
+    valuesCnt = len(c)
+
+    A_tmp = copyMatr(A)
+
+    # change all limits expressions
+    for i in range(limitsCnt):
+        if (sign_limits[i] == ">=" and type_opt == "max") or (sign_limits[i] == "<=" and type_opt == "min"):
+            if sign_limits[i] == ">=":
+                sign_limits[i] = "<="
+            else:
+                sign_limits[i] = ">="
+
+            for j in range(len(A_tmp[i])):
+                A_tmp[i][j] = -1 * A_tmp[i][j]
+            b[i] = b[i] * -1
+
+    # set dual target extremum
+    if type_opt == "min":
+        type_opt_dual = "max"
+        expr_d_sign = "<="
+    else:
+        type_opt_dual = "min"
+        expr_d_sign = ">="
+
+    # swap b and c
+    c_d = b
+    b_d = c
+
+    # trasponate
+    for i in range(len(A_tmp[0])):
+        A_dual.append(takeColumn(A_tmp, i))
+
+    # set limits signs
+    for i in range(len(c)):
+        if i in value_limits:
+            limSigns_dual.append(expr_d_sign)
+        else:
+            limSigns_dual.append("=")
+
+    # set values signs
+    for i in range(limitsCnt):
+        if sign_limits[i] != "=":
+            valuesLimits_dual.append(i)
+
+    return A_dual, b_d, c_d, limSigns_dual, type_opt_dual, valuesLimits_dual
+
+
+def print_task_as_dual(A_d: list, b_d: list, c_d: list, v_d: float, limSigns_d: list, extrSign_d: str,
+                       valuesLimits_d: list):
+    str_to_out = extrSign_d + ": target = " + str(v_d)
+    for i in range(len(c_d)):
+        str_to_out += " + " + str(c_d[i]) + " * y" + str(i + 1)
+    print(str_to_out)
+
+    for i in range(len(A_d)):
+        str_to_out = ""
+        for j in range(len(A_d[i])):
+            str_to_out += str(A_d[i][j]) + " * y" + str(j + 1)
+            if j != len(A_d[i]) - 1:
+                str_to_out += " + "
+        str_to_out += " " + limSigns_d[i] + " " + str(b_d[i])
+        print(str_to_out)
+
+    for i in range(len(c_d)):
+        if i in valuesLimits_d:
+            str_to_out = "y" + str(i + 1) + " >= 0"
+            print(str_to_out)
+
+    return
